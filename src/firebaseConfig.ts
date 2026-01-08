ba44c6fd233f0693580762fdd88a1b5c36ace169
kamranShah-Dev <mrbukhari117@gmail.com>
2026-01-08 17:18:20 +0500

    security check

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -6,12 +6,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBqR7KOYfzrMjE_jnffTALPsJeYRRYjs4Y",
-  authDomain: "katha-app-db.firebaseapp.com",
-  projectId: "katha-app-db",
-  storageBucket: "katha-app-db.firebasestorage.app",
-  messagingSenderId: "707210404448",
-  appId: "1:707210404448:web:3450fdc7699f0ba5fc5905"
+  
 };
 
 // Initialize Firebase
